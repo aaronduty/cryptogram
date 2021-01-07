885b47a0c6dba98fc0a59b98e6c8967d158d33d0
Aaron Duty <aaron@eksdyne.com>
2021-01-07 09:19:58 -0500

    Add a separate Home controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # api endpoints
   get '/cryptogram', to: 'cryptogram#show'
   post '/cryptogram/check', to: 'cryptogram#check'
+
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
