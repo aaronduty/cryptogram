9c0be03731b0d767c123a1f8fa304b00a957e9dc
Aaron Duty <aaron@eksdyne.com>
2021-01-07 07:06:08 -0500

    Add the initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/cryptogram', to: 'cryptogram#show'
+  post '/cryptogram/check', to: 'cryptogram#check'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
